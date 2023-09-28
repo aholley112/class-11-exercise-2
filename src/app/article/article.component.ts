ce1e9dae5db7944b66e89e3dcc7be45c5b985cee
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-28 15:28:19 -0500

    Exercise 2
    Class 11 Exercise 2

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+  @Input() title: string = "Whatever you want";
+  @Input() content: string = "Some content goes here";
+  isTechRelated: boolean = true;
+}
